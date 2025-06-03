6bf640703b60dec9b3b5b2f85a4cc3c9d77cbec6
Boots <61475589+DeBoots-Nkuna@users.noreply.github.com>
2025-06-03 21:34:47 +0200

    Update validation functionality
    - Modified and improved validation method

--- frontend/src/utils/validateSurveyForm.js
+++ frontend/src/utils/validateSurveyForm.js
@@ -6,38 +6,45 @@ export const validateSurveyForm = (formData) => {
   const dateOfBirth = formData.get('dateOfBirth')
   const contactNumber = formData.get('contactNumber')
 
-  //checking for blank input fields in personal details
-  if (!fullName || !email || !dateOfBirth || !contactNumber) {
-    return 'Please fill in all personal details'
+  //checking for dateOfBirth
+
+  if (!dateOfBirth) {
+    return 'Please enter your date of birth.'
   }
 
-  //validating if user's age is within range NOT < 5 and NOT > 120
+  //calculating age
   const currentDate = new Date()
   const birthYear = new Date(dateOfBirth)
 
   const age = currentDate.getFullYear() - birthYear.getFullYear()
 
+  //checking for age restriction
   if (age < 5 || age > 120) {
     return 'Age must be between 5 and 120.'
+  } else {
+    //proceeding with validation when age range is met
+
+    if (!fullName || !email || !dateOfBirth || !contactNumber) {
+      return 'Please fill in all personal details'
+    }
+
+    //validating if at least one check box is selected
+    let favMeals = formData.getAll('favoriteFood')
+    if (favMeals.length === 0) {
+      return 'Please select at least one favorite meal.'
+    }
+
+    //checking if user selected a rating for each question
+    const movies = Number(formData.get('question1'))
+    const radio = Number(formData.get('question2'))
+    const eatOut = Number(formData.get('question3'))
+    const tv = Number(formData.get('question4'))
+
+    if (!movies || !radio || !eatOut || !tv) {
+      return 'please answer all rating questions.'
+    }
+
+    //returning null when all conditions are met
+    return null
   }
-
-  let favMeals = formData.getAll('favoriteFood')
-
-  //checking if user checked at least one check box
-  if (favMeals.length === 0) {
-    return 'Please select at least one favorite meal.'
-  }
-
-  //checking if user selected a rating for each question
-  const movies = Number(formData.get('question1'))
-  const radio = Number(formData.get('question2'))
-  const eatOut = Number(formData.get('question3'))
-  const tv = Number(formData.get('question4'))
-
-  if (!movies || !radio || !eatOut || !tv) {
-    return 'please answer all rating questions.'
-  }
-
-  //returning null when all conditions are met
-  return null
 }
